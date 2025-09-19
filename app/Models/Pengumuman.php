d693ce3fc865e7cfc57bd7f31495b2e83a443dcb
Muhammad Irvansyah <mirvansyah132@gmail.com>
2025-09-19 14:28:09 +0700

    migrasition-filament dasbord

--- app/Models/Pengumuman.php
+++ app/Models/Pengumuman.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Iluminate\Database\Elloquent\Factories\HasFactory
 use Illuminate\Database\Eloquent\Model;
 
 class Pengumuman extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'pengumuman';
+
+    protected $fillable = [
+        'judul',
+        'isi',
+        'tanggal_publikasi',
+        'status_publikasi',
+
+    ];
+
+    protected $casts = [
+        'tanggal_publikasi' => 'date',
+        'status_publikasi', => 'boolean',
+    ];
 }
